7a368673308618c87c39245c0de378c69a67c7d7
LiberRG <libertuty@hotmail.com>
2023-10-23 01:30:19 +0200

    actualización header y footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,14 +1,85 @@
-<footer class="bg-azul-claro">
-    <div class="container h-14 flex flex-row justify-between items-center ">
+<footer class="bg-azul-claro relative" x-data="footer()">
+    <div class="container h-14 flex flex-row justify-between items-center">
         <div class="grow text-blanco">
             @svg("logo-anacos-galicia", "h-12 w-auto hidden lg:inline")
-            @svg("logo-anacos", "h-6 w-auto lg:hidden")            
+            @svg("logo-anacos", "h-6 w-auto lg:hidden")
         </div>
         <div class="flex justify-between gap-3">
-            <a class="text-sm" href="">@svg("ico-cookie", "h-9 w-auto")</a>
-            <a class="text-sm" href="">@svg("ico-ubicacion", "h-9 w-auto")</a>
-            <a class="text-sm" href="">@svg("ico-telefono", "h-9 w-auto")</a>
-            <a class="text-sm" href="">@svg("ico-instagram", "h-9 w-auto")</a>
+            <button @click="desplega('cookie')" class="text-sm">
+                @svg("ico-cookie", "h-9 w-auto")
+            </button>
+            <button @click="desplega('contacto')" class="text-sm">
+                <span>@svg("ico-ubicacion", "h-9 w-auto")</span>
+            </button>
+            <a class="text-sm" href="https://instagram.com/anacos.gal?igshid=MzRlODBiNWFlZA==">
+                @svg("ico-instagram", "h-9 w-auto")
+            </a>
         </div>
     </div>
+
+    <div class="bg-azul-claro/[0.8] font-normal absolute right-0 bottom-14 w-full text-start text-blanco z-10 ">
+        <div class="container flex justify-end">
+
+            <div x-bind:class="cookie ? 'py-4' :'hidden'">
+                <h3 class="h3 font-bold py-2 pl-3">Uso de cookies</h3>
+                <div class="sm:flex items-center gap-x-4" >
+                    <p class="p">Este portal utiliza cookies propias de tipo técnico y de terceros para mejorar, mediante el análisis de la navegación, el servicio ofrecido. Las cookies no se utilizan para recoger datos personales.</p>
+                    <div class="flex flex-col gap-y-3">
+                        <button class="boton w-full text-magenta-oscuro border-magenta-oscuro">Opciones</button>
+                        <button class="boton w-full text-verde-oscuro border-verde-oscuro">Acepto</button>
+                    </div>
+                </div>                
+            </div>
+
+            <div x-bind:class="contacto ? 'py-4' :'hidden'">
+                <div class="flex items-center justify-between gap-x-4" >
+                    <div>
+                        <h3 class="h3 font-bold py-2 pl-3">Contacta con nosotros:</h3>
+                        <p class="p"><b>Telefono:</b>  +34 000 000 000</p>
+                        <p class="p"><b>Email:</b>  info@anacos.com</p>
+                        <p class="p"><b>Dirección:</b>  Rúa do Mar nº22<br>Combarro, Pontevedra - C.P.:36993</p>
+                    </div>
+                    <iframe class="w-1/3 h-auto border-4 border-blanco" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d184.0496775212856!2d-8.704583534417495!3d42.43210163961175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd2f6ef58e9a133f%3A0x963c1473b696d4f8!2sR%C3%BAa%20do%20Mar%2C%2016%2C%2036993%20Combarro%2C%20Pontevedra!5e0!3m2!1ses!2ses!4v1698016102188!5m2!1ses!2ses" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </footer>
+
+<script>
+    function footer() {
+        return {
+            cookie: false,
+            contacto: false,
+            instagram: false,
+
+            desplega(event) {
+                switch (event) {
+                    case 'cookie':
+                        this.cookie = !this.cookie
+                        this.contacto = false
+                        this.instagram = false
+                        break;
+                    case 'contacto':
+                        this.cookie = false
+                        this.contacto = !this.contacto
+                        this.instagram = false
+                        break;
+                    case 'instagram':
+                        this.cookie = false
+                        this.contacto = false
+                        this.instagram = !this.instagram
+                        break;                   
+                    default:
+                        this.cookie = false
+                        this.contacto = false
+                        this.instagram = false
+                        this.ubicacion = false
+                        break;
+                }
+
+            }
+        }
+    }
+</script>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,5 @@
-{# arreglar el desplegable movil para que todas bajen en el mismo momento, no usar en los div de categorias min-w-max #}
-
-
-
 <div x-data="{ open: false }">
+    <!--arreglar el desplegable movil para que todas bajen en el mismo momento, no usar en los div de categorias min-w-max -->
     <div x-data="header()">
         <nav class="bg-azul-claro flex flex-row justify-between items-center py-3 z-50 sticky sm:px-8 px-4">
             <a href="/" class="bg-blanco rounded-full border-4 border-blanco">@svg("isotipo-anacos", 'w-12 h-auto')</a>
@@ -10,72 +7,72 @@
             <div x-cloak class="md:inline text-blanco" x-bind:class="open ? 'inline-block':'hidden'">
                 <div class="md:static md:inline w-full md:text-center grow" x-bind:class="open ? 'absolute right-0 top-20 text-start bg-azul-claro/[0.8] z-50 ' : '' ">
     
-                    <spam  class="flex items-start gap-6 flex-wrap md:inline
+                    <spam  class="flex items-start gap-x-6 flex-wrap md:inline
                         text-lg font-bold md:font-normal
-                        p-3 xl:px-8 2xl:px-10">
-                        <button name="planea" @click="desplegable($event)" class="min-w-[25%] md:min-w-max hover:md:text-magenta-oscuro" x-bind:class="planea ? 'md:text-magenta-oscuro' : 'md:text-blanco'">
+                        p-5 xl:px-8 2xl:px-10">
+                        <button name="planea" @click="desplegable($event)" class="uppercase min-w-[25%] md:min-w-max hover:md:text-magenta-oscuro" x-bind:class="planea ? 'md:text-magenta-oscuro' : 'md:text-blanco'">
                             Planea
                         </button>
                         <div class="font-normal min-w-max flex flex-col
                         text-start " x-bind:class="planea ? 'md:absolute md:right-0 md:top-20 md:bg-azul-claro/[0.8] md:z-50 md:w-2/5 lg:w-1/3' :'md:hidden'">
-                            <a class="hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
+                            <a class="p hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
                                 Alojamiento
                             </a>
-                            <a class="hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
+                            <a class="p hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
                                 Servicios turísticos
                             </a>
-                            <a class="hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
+                            <a class="p hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
                                 Como llegar
                             </a>
-                            <a class="hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
+                            <a class="p hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
                                 Cultura y tradiciones
                             </a>
-                            <a class="hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
+                            <a class="p hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
                                 Sabores del lugar
                             </a>
-                            <a class="hover:text-azul-oscuro md:p-5" href="">
+                            <a class="p hover:text-azul-oscuro md:p-5" href="">
                                 Paisajes
                             </a>
                         </div>
                     </spam>
-                    <spam class=" flex gap-6 items-start flex-wrap md:inline
+                    <spam class=" flex gap-x-6 items-start flex-wrap md:inline
                         text-lg font-bold md:font-normal
-                        p-3 xl:px-8 2xl:px-10">
-                        <button name="inspira" @click="desplegable($event)" class="min-w-[25%] md:min-w-max hover:md:text-magenta-oscuro" x-bind:class="inspira ? 'md:text-magenta-oscuro' : 'md:text-blanco'">
+                        p-5 xl:px-8 2xl:px-10">
+                        <button name="inspira" @click="desplegable($event)" class="uppercase min-w-[25%] md:min-w-max hover:md:text-magenta-oscuro" x-bind:class="inspira ? 'md:text-magenta-oscuro' : 'md:text-blanco'">
                             Inspirate
                         </button>
                         <div class="font-normal min-w-max flex flex-col
                         text-start " x-bind:class="inspira ? 'md:absolute md:right-0 md:top-20 md:bg-azul-claro/[0.8] md:z-50 md:w-2/5 lg:w-1/3' :'md:hidden'">
-                            <a class="hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
+                            <a class="p hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
                                 Colección Galiza máxica
                             </a>
-                            <a class="hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
+                            <a class="p hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
                                 Colección Sons
                             </a>
-                            <a class="hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
+                            <a class="p hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
                                 Colección Ollar da natureza
                             </a>
-                            <a class="hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
+                            <a class="p hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
                                 colección A liberdade do toxo
                             </a>
-                            <a class="hover:text-azul-oscuro md:p-5" href="">
+                            <a class="p hover:text-azul-oscuro md:p-5" href="">
                                 Colección Combarro
                             </a>
                         </div>
                     </spam>
-                    <spam class=" flex gap-6 items-start flex-wrap md:inline
+                    <spam class=" flex gap-x-6 items-start flex-wrap md:inline
                         text-lg font-bold md:font-normal
-                        p-3 xl:px-8 2xl:px-10">
-                        <button name="juega" @click="desplegable($event)" class="min-w-[25%] md:min-w-max hover:md:text-magenta-oscuro" x-bind:class="juega ? 'md:text-magenta-oscuro' : 'md:text-blanco'">
+                        p-5 xl:px-8 2xl:px-10">
+                        <button name="juega" @click="desplegable($event)" class="uppercase min-w-[25%] md:min-w-max hover:md:text-magenta-oscuro" x-bind:class="juega ? 'md:text-magenta-oscuro' : 'md:text-blanco'">
                             Descubre
                         </button>
                         <div class="font-normal min-w-max flex flex-col
                         text-start " x-bind:class="juega ? 'md:absolute md:right-0 md:top-20 md:bg-azul-claro/[0.8] md:z-50 md:w-2/5 lg:w-1/3' :'md:hidden'">
-                            <a class="hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
+                            <a class="p hover:text-azul-oscuro md:p-5 md:border-b md:border-blanco" href="{{ route('events.index') }}">
                                 Empareja y descubre
                             </a>
     
-                            <a class="hover:text-azul-oscuro md:p-5" href="">
+                            <a class="p hover:text-azul-oscuro md:p-5" href="">
                                 Otro
                             </a>
                         </div>
