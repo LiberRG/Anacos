753e4115035b9f52fa8f93398a168953ca4628f1
LiberRG <libertuty@hotmail.com>
2023-12-17 16:27:02 +0100

    .

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="area-card cursor-pointer inline-block m-2 w-36 xs:w-32 sm:w-44 md:w-36 lg:w-48">
+<div class="area-card cursor-pointer m-2 w-36 xs:w-32 sm:w-44 md:w-36 lg:w-48">
     <div class="card cursor-pointer relative inline-block" id="{{$id}}" style="height:0;width:100%;padding-bottom:125%;transform: rotateY(0deg);">
         <div class="drowCard w-full h-full absolute flex justify-center">
             <img class="w-full rounded-xl border-8 border-azul-oscuro" src="{{$path}}">
